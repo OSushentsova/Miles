06769069adb5d14b939187f1a4a40cee1cc63398
OSushentsova@LAPTOP-RLQHCN06 <yaraindrop@gmail.com>
2020-08-24 22:38:45 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+            double amount = 10758.60;
+            int miles = (int) (amount / 20);
+            System.out.println(miles);
+        }
+}
